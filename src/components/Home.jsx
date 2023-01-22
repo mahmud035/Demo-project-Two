0058cd1b62a2548902879e78523c095b22428fe9
mahmud035 <mahamudulhasan4148@gmail.com>
2023-01-22 23:59:44 +0600

    added border

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,8 +7,12 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-      <Profile />
-      <Table />
+      <div className="max-w-screen-lg mx-auto pb-10  pt-4">
+        <div className="h-fit container sm:mx-auto rounded-3xl sm:border-2 sm:border-gray-200 ">
+          <Profile />
+          <Table />
+        </div>
+      </div>
     </div>
   );
 };
